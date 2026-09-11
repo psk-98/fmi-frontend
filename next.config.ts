488434d9ac0978f25fa685b67801d0e78420e048
Paul <paul@vetro.co.za>
2026-09-11 22:02:17 +0200

    add compose, eco for deployment options

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  output: process.env.NEXT_STANDALONE === 'true' ? 'standalone' : undefined,
+  // output: 'standalone',
   poweredByHeader: false,
-};
+}
 
-export default nextConfig;
+export default nextConfig
